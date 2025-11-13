b97ae317ff5904ab478ed69985ab5a0fc8aed4f4
Santosh <santosh@Santoshs-MacBook-Air.local>
2025-11-13 15:53:10 +0530

    Login error

--- api/index.js
+++ api/index.js
@@ -1,3 +1,16 @@
 const app = require('../index.js');
 
-module.exports = app;
+// Vercel serverless function handler
+module.exports = (req, res) => {
+  // Set CORS headers for preflight requests
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.status(200).end();
+    return;
+  }
+  
+  // Pass the request to the Express app
+  return app(req, res);
+};
